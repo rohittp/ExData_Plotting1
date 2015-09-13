72acab634e79c28d739f42097724e33172026129
rohittp <rohittp@gmail.com>
2015-09-13 13:24:04 -0400

    Updated version

--- plot1.R
+++ plot1.R
@@ -5,8 +5,10 @@ wd<-getwd()
 # get file path of file household_power_consumption.txt 
 filepath<-file.path(wd,"household_power_consumption.txt",fsep = .Platform$file.sep)
 # read file using sqldf. Only read rows where dates are 1/2/2007 or 2/2/2007
-x<-read.csv2.sql(filepath,sql="select * from file 
-where Date in('1/2/2007','2/2/2007')",header = TRUE,na.strings ="?")
+x<-read.csv.sql(filepath,sql="select * from file 
+where Date in('1/2/2007','2/2/2007')",header = TRUE,sep=";")
+#Replace missing values with NA
+x[x=="?"]=NA
 # add column datetime which is a concatenation of Date & Time column
 x<-mutate(x,datetime=paste(Date,Time))
 # convert datetime column to POSIXlt using strptime
@@ -15,3 +17,4 @@ x$datetime<-strptime(x$datetime,"%d/%m/%Y %H:%M:%S")
 png(filename="plot1.png")
 hist(x$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
 dev.off()
+
--- plot3.R
+++ plot3.R
@@ -5,8 +5,10 @@ wd<-getwd()
 # get file path of file household_power_consumption.txt 
 filepath<-file.path(wd,"household_power_consumption.txt",fsep = .Platform$file.sep)
 # read file using sqldf. Only read rows where dates are 1/2/2007 or 2/2/2007
-x<-read.csv2.sql(filepath,sql="select * from file 
-where Date in('1/2/2007','2/2/2007')",header = TRUE,na.strings ="?")
+x<-read.csv.sql(filepath,sql="select * from file 
+where Date in('1/2/2007','2/2/2007')",header = TRUE)
+#Replace missing values with NA
+x[x=="?"]=NA
 # add column datetime which is a concatenation of Date & Time column
 x<-mutate(x,datetime=paste(Date,Time))
 # convert datetime column to POSIXlt using strptime
